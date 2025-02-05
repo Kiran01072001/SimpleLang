0fda477a692ed43ab942ab3041deafc2a23dfb15
Maradana Kiran <131331920+Kiran01072001@users.noreply.github.com>
2025-02-05 18:53:37 +0530

    Update Lexical.c

--- Simplelang Compiler/Lexical.c
+++ Simplelang Compiler/Lexical.c
@@ -3,19 +3,25 @@
 #include <string.h>
 #include <ctype.h>
 
-#define MAX_TOKENS 100
+// We can define maximum number of tokens as per require.
 
-typedef struct {
-    char type[20];
-    char value[20];
+#define MAX_TOKENS 120
+
+typedef struct 
+{
+    char type[25];
+    char value[25];
 } Token;
 
-Token tokens[MAX_TOKENS];
+Token tokens[MAX_TOKENS]; // Array for store tokens
+
 int token_count = 0;
 
-void add_token(const char *type, const char *value) {
-    strcpy(tokens[token_count].type, type);
-    strcpy(tokens[token_count].value, value);
+void plus_token(const char *type, const char *value) 
+{
+    strncpy(tokens[token_count].type, type);
+    
+    strncpy(tokens[token_count].value, value);
     token_count++;
 }
 
@@ -27,23 +33,35 @@ void tokenize(const char *code) {
             buffer[j++] = code[i];
             while (isdigit(code[++i])) buffer[j++] = code[i];
             buffer[j] = '\0';
-            add_token("NUMBER", buffer);
+            plus_token("NUMBER", buffer);
         } else if (isalpha(code[i])) {
             buffer[j++] = code[i];
             while (isalnum(code[++i])) buffer[j++] = code[i];
             buffer[j] = '\0';
-            if (strcmp(buffer, "int") == 0) add_token("INT", buffer);
-            else if (strcmp(buffer, "if") == 0) add_token("IF", buffer);
-            else add_token("IDENTIFIER", buffer);
+            if (strcmp(buffer, "int") == 0) plus_token("INT", buffer);
+            else if (strcmp(buffer, "if") == 0) plus_token("IF", buffer);
+            else plus_token("IDENTIFIER", buffer);
         } else if (code[i] == '=') {
-            if (code[i + 1] == '=') { add_token("EQUAL", "=="); i++; }
-            else add_token("ASSIGN", "=");
-        } else if (code[i] == '+') add_token("PLUS", "+");
-        else if (code[i] == ';') add_token("SEMICOLON", ";");
-        else if (code[i] == '{') add_token("LBRACE", "{");
-        else if (code[i] == '}') add_token("RBRACE", "}");
-        else if (code[i] == '(') add_token("LPAREN", "(");
-        else if (code[i] == ')') add_token("RPAREN", ")");
+            if (code[i + 1] == '=') { plus_token("EQUAL", "=="); i++; }
+            else plus_token("ASSIGN", "=");
+        } else if
+        // character is a plus symbol
+        (code[i] == '+') plus_token("PLUS", "+");
+        else if
+        // character is a ;
+        (code[i] == ';') plus_token("SEMICOLON", ";");
+        else if
+        //character is a { Lbrace
+        (code[i] == '{') plus_token("LBRACE", "{");
+        else if 
+        //character is a } Rbrace
+        (code[i] == '}') plus_token("RBRACE", "}");
+        else if 
+        // character is a (  Lparen
+        (code[i] == '(') plus_token("LPAREN", "(");
+        else if 
+        // character is a ) Rparen
+        (code[i] == ')') plus_token("RPAREN", ")");
         i++, j = 0;
     }
 }
