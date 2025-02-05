df0d7d9f709e9e42cd55db0b10b3509b7e45cec8
Maradana Kiran <131331920+Kiran01072001@users.noreply.github.com>
2025-02-05 19:13:40 +0530

    Update Codegen.c

--- Simplelang Compiler/Codegen.c
+++ Simplelang Compiler/Codegen.c
@@ -1,18 +1,34 @@
 #include "Parsing.c"
 
-void generate_assembly() {
+void generate_assembly() 
+{
     FILE *file = fopen("output.asm", "w");
-    for (int i = 0; i < ast_count; i++) {
-        if (strcmp(ast[i]->type, "DECLARATION") == 0) {
-            fprintf(file, "%s DB 0\n", ast[i]->value);
-        } else if (strcmp(ast[i]->type, "ASSIGNMENT") == 0) {
-            fprintf(file, "MOV %s, %s\n", ast[i]->value, ast[i]->children[0]->value);
-        } else if (strcmp(ast[i]->type, "IF_STATEMENT") == 0) {
-            fprintf(file, "CMP %s, 30\nJNZ END_IF\n", ast[i]->children[0]->value);
-            fprintf(file, "MOV %s, %s\n", ast[i]->children[1]->children[0]->value,
-                    ast[i]->children[1]->children[0]->children[0]->value);
+
+    
+    for (int index = 0; index < node_count; index++) 
+    {
+        if (strcmp(ast[index]->type, "DECLARATION") == 0) 
+        {
+            fprintf(file, "%s DB 0\n", ast[index]->value);
+        } else if (strcmp(ast[index]->type, "ASSIGNMENT") == 0) 
+        
+        {
+            fprintf(file, "MOV %s, %s\n", ast[index]->value, ast[index]->children[0]->value);
+        } else if (strcmp(ast[index]->type, "IF_STATEMENT") == 0) 
+        
+        {
+            fprintf(file, "CMP %s, 30\nJNZ END_IF\n", ast[index]->children[0]->value);
+
+            
+            fprintf(file, "MOV %s, %s\n", ast[index]->children[1]->children[0]->value,
+                    ast[index]->children[1]->children[0]->children[0]->value);
+
+            
             fprintf(file, "END_IF:\n");
         }
     }
+
+
+    
     fclose(file);
 }
