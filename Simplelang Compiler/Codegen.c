e07ee05536d663704f7510548d1caf57c1bc6c1f
Maradana Kiran <131331920+Kiran01072001@users.noreply.github.com>
2025-02-05 19:29:44 +0530

    Update Codegen.c

--- Simplelang Compiler/Codegen.c
+++ Simplelang Compiler/Codegen.c
@@ -2,7 +2,7 @@
 
 void generate_assembly() 
 {
-    FILE *file = fopen("output.asm", "w");
+    FILE *file = fopen("output.asm", "w"); // Automatically generated once compile
 
     
     for (int index = 0; index < node_count; index++) 
@@ -10,11 +10,16 @@ void generate_assembly()
         if (strcmp(ast[index]->type, "DECLARATION") == 0) 
         {
             fprintf(file, "%s DB 0\n", ast[index]->value);
-        } else if (strcmp(ast[index]->type, "ASSIGNMENT") == 0) 
+        } 
+        
+        
+        else if (strcmp(ast[index]->type, "ASSIGNMENT") == 0) 
         
         {
             fprintf(file, "MOV %s, %s\n", ast[index]->value, ast[index]->children[0]->value);
-        } else if (strcmp(ast[index]->type, "IF_STATEMENT") == 0) 
+        } else if 
+            
+            (strcmp(ast[index]->type, "IF_STATEMENT") == 0) 
         
         {
             fprintf(file, "CMP %s, 30\nJNZ END_IF\n", ast[index]->children[0]->value);
