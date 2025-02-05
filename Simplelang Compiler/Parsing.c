2ab4e52847241f7c7f2dc1eb9332a037c37bc815
Maradana Kiran <131331920+Kiran01072001@users.noreply.github.com>
2025-02-05 19:06:32 +0530

    Update Parsing.c

--- Simplelang Compiler/Parsing.c
+++ Simplelang Compiler/Parsing.c
@@ -9,7 +9,8 @@ typedef struct ASTNode {
 ASTNode *ast[MAX_TOKENS];
 int ast_count = 0;
 
-ASTNode *create_node(const char *type, const char *value) {
+ASTNode *create_ast_node(const char *type, const char *value)
+{
     ASTNode *node = (ASTNode *)malloc(sizeof(ASTNode));
     strcpy(node->type, type);
     strcpy(node->value, value);
@@ -18,37 +19,43 @@ ASTNode *create_node(const char *type, const char *value) {
 }
 
 void parse() {
-    int i = 0;
-    while (i < token_count) {
-        if (strcmp(tokens[i].type, "INT") == 0) {
-            ast[ast_count++] = create_node("DECLARATION", tokens[i + 1].value);
-            i += 2;
-        } else if (strcmp(tokens[i].type, "IDENTIFIER") == 0 && strcmp(tokens[i + 1].type, "ASSIGN") == 0) {
-            ASTNode *node = create_node("ASSIGNMENT", tokens[i].value);
-            node->children[0] = create_node("EXPR", tokens[i + 2].value);
-            ast[ast_count++] = node;
-            i += 4;
-        } else if (strcmp(tokens[i].type, "IF") == 0) {
-            ASTNode *condition = create_node("CONDITION", tokens[i + 2].value);
-            ASTNode *block = create_node("BLOCK", "");
-            i += 4;
-            while (strcmp(tokens[i].type, "RBRACE") != 0) {
-                if (strcmp(tokens[i].type, "IDENTIFIER") == 0 && strcmp(tokens[i + 1].type, "ASSIGN") == 0) {
-                    ASTNode *stmt = create_node("ASSIGNMENT", tokens[i].value);
-                    stmt->children[0] = create_node("EXPR", tokens[i + 2].value);
+    int index = 0;
+    while (index < token_count) {
+        if (strcmp(tokens[index].type, "INT") == 0) {
+            ast[node_count++] = create_ast_node("DECLARATION", tokens[index + 1].value);
+            index += 2;
+        } else if (strcmp(tokens[index].type, "IDENTIFIER") == 0 && strcmp(tokens[index + 1].type, "ASSIGN") == 0)
+        {
+            ASTNode *node = create_ast_node("ASSIGNMENT", tokens[index].value);
+            node->children[0] = create_ast_node("EXPR", tokens[index + 2].value);
+            ast[node_count++] = node;
+            index += 4;
+        } else if (strcmp(tokens[index].type, "IF") == 0) {
+            ASTNode *condition = create_ast_node("CONDITION", tokens[index + 2].value);
+            ASTNode *block = create_ast_node("BLOCK", "");
+            index += 4;
+            while (strcmp(tokens[index].type, "RBRACE") != 0) {
+                if (strcmp(tokens[index].type, "IDENTIFIER") == 0 && strcmp(tokens[index + 1].type, "ASSIGN") == 0)
+                {
+                    ASTNode *stmt = create_ast_node("ASSIGNMENT", tokens[index].value);
+                    stmt->children[0] = create_ast_node("EXPR", tokens[index + 2].value);
                     block->children[0] = stmt;
-                    i += 4;
-                } else {
-                    i++;
+                    index += 4;
+                } else
+                
+                {
+                    index++;
                 }
             }
-            ASTNode *if_stmt = create_node("IF_STATEMENT", "");
+            ASTNode *if_stmt = create_ast_node("IF_STATEMENT", "");
             if_stmt->children[0] = condition;
             if_stmt->children[1] = block;
             ast[ast_count++] = if_stmt;
-            i++;
-        } else {
-            i++;
+            index++;
+        } else 
+        
+        {
+            index++;
         }
     }
 }
