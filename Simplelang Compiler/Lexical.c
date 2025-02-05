a078cbaed725b4eef8d80591b4140c19d75d0403
Maradana Kiran <131331920+Kiran01072001@users.noreply.github.com>
2025-02-05 19:16:29 +0530

    Update Lexical.c

--- Simplelang Compiler/Lexical.c
+++ Simplelang Compiler/Lexical.c
@@ -27,41 +27,41 @@ void plus_token(const char *type, const char *value)
 
 void tokenize(const char *code) {
     char buffer[20];
-    int i = 0, j = 0;
-    while (code[i] != '\0') {
-        if (isdigit(code[i])) {
-            buffer[j++] = code[i];
-            while (isdigit(code[++i])) buffer[j++] = code[i];
+    int index = 0, j = 0;
+    while (code[index] != '\0') {
+        if (isdigit(code[index])) {
+            buffer[j++] = code[index];
+            while (isdigit(code[++index])) buffer[j++] = code[index];
             buffer[j] = '\0';
             plus_token("NUMBER", buffer);
-        } else if (isalpha(code[i])) {
-            buffer[j++] = code[i];
-            while (isalnum(code[++i])) buffer[j++] = code[i];
+        } else if (isalpha(code[index])) {
+            buffer[j++] = code[index];
+            while (isalnum(code[++index])) buffer[j++] = code[index];
             buffer[j] = '\0';
             if (strcmp(buffer, "int") == 0) plus_token("INT", buffer);
             else if (strcmp(buffer, "if") == 0) plus_token("IF", buffer);
             else plus_token("IDENTIFIER", buffer);
-        } else if (code[i] == '=') {
-            if (code[i + 1] == '=') { plus_token("EQUAL", "=="); i++; }
+        } else if (code[index] == '=') {
+            if (code[index + 1] == '=') { plus_token("EQUAL", "=="); index++; }
             else plus_token("ASSIGN", "=");
         } else if
         // character is a plus symbol
-        (code[i] == '+') plus_token("PLUS", "+");
+        (code[index] == '+') plus_token("PLUS", "+");
         else if
         // character is a ;
-        (code[i] == ';') plus_token("SEMICOLON", ";");
+        (code[index] == ';') plus_token("SEMICOLON", ";");
         else if
         //character is a { Lbrace
-        (code[i] == '{') plus_token("LBRACE", "{");
+        (code[index] == '{') plus_token("LBRACE", "{");
         else if 
         //character is a } Rbrace
-        (code[i] == '}') plus_token("RBRACE", "}");
+        (code[index] == '}') plus_token("RBRACE", "}");
         else if 
         // character is a (  Lparen
-        (code[i] == '(') plus_token("LPAREN", "(");
+        (code[index] == '(') plus_token("LPAREN", "(");
         else if 
         // character is a ) Rparen
-        (code[i] == ')') plus_token("RPAREN", ")");
-        i++, j = 0;
+        (code[index] == ')') plus_token("RPAREN", ")");
+        index++, j = 0;
     }
 }
