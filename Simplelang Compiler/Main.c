877f7017556168cf8dc85687f1bd7fc3773fdb44
Maradana Kiran <131331920+Kiran01072001@users.noreply.github.com>
2025-02-05 19:23:08 +0530

    Update Main.c

--- Simplelang Compiler/Main.c
+++ Simplelang Compiler/Main.c
@@ -1,22 +1,32 @@
 #include "Codegen.c"
 
-void read_file(const char *filename, char *buffer) {
+void read_file(const char *filename, char *buffer) 
+
+{
     FILE *file = fopen(filename, "r");
     if (!file) {
-        printf("Error opening file.\n");
+        printf("Can not open the file.\n");
+
+
+        
         exit(1);
     }
+
+    
     fread(buffer, sizeof(char), 1000, file);
+    
     fclose(file);
 }
 
-int main() {
+int main() 
+
+{
     char code[1000];
     read_file("input.slang", code);
     tokenize(code);
     parse();
     generate_assembly();
-    printf("Assembly code generated in output.asm\n");
+    printf("Assembly code generating in output.asm\n");
     return 0;
 }
 
