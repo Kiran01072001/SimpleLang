813d0f260d24fe14f464ad5822a38a908f823d08
Maradana Kiran <131331920+Kiran01072001@users.noreply.github.com>
2025-02-06 09:44:18 +0530

    Update Codegen.c

--- Simplelang Compiler/Codegen.c
+++ Simplelang Compiler/Codegen.c
@@ -3,14 +3,14 @@
 void generate_assembly() 
 {   
     // writing the assembly instructions
-    FILE *file = fopen("output.asm", "w"); // Automatically generated once compile
+    FILE *assemblyFile = fopen("output.asm", "w"); // Automatically generated once compile
 
     
     for (int index = 0; index < node_count; index++) 
     {
         if (strcmp(ast[index]->type, "DECLARATION") == 0) 
         {
-            fprintf(file, "%s DB 0\n", ast[index]->value);
+            fprintf(assemblyFile, "%s DW 0\n", ast[index]->value);
         } 
         
         
@@ -23,18 +23,18 @@ void generate_assembly()
             (strcmp(ast[index]->type, "IF_STATEMENT") == 0) 
         
         {
-            fprintf(file, "CMP %s, 30\nJNZ END_IF\n", ast[index]->children[0]->value);
+            fprintf(assemblyFile, "CMP %s, 30\nJNZ END_IF\n", ast[index]->children[0]->value);
 
             
-            fprintf(file, "MOV %s, %s\n", ast[index]->children[1]->children[0]->value,
+            fprintf(assemblyFile, "MOV %s, %s\n", ast[index]->children[1]->children[0]->value,
                     ast[index]->children[1]->children[0]->children[0]->value);
 
             
-            fprintf(file, "END_IF:\n");
+            fprintf(assemblyFile, "END_IF:\n");
         }
     }
 
 
     
-    fclose(file);
+    fclose(assemblyFile);
 }
