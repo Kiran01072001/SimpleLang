c96a9526640fce419f06e4912c71b281db2fd49e
Maradana Kiran <131331920+Kiran01072001@users.noreply.github.com>
2025-02-06 09:47:26 +0530

    Update Lexical.c

--- Simplelang Compiler/Lexical.c
+++ Simplelang Compiler/Lexical.c
@@ -25,23 +25,31 @@ void plus_token(const char *type, const char *value)
     token_count++;
 }
 
-void tokenize(const char *code) {
+void tokenize(const char *code)
+{
     char buffer[20];
     int index = 0, j = 0;
-    while (code[index] != '\0') {
-        if (isdigit(code[index])) {
+    while (code[index] != '\0')
+        {
+        if (isdigit(code[index]))
+        {
             buffer[j++] = code[index];
             while (isdigit(code[++index])) buffer[j++] = code[index];
             buffer[j] = '\0';
             plus_token("NUMBER", buffer);
-        } else if (isalpha(code[index])) {
+        } else if (isalpha(code[index])) 
+        
+        
+        {
             buffer[j++] = code[index];
             while (isalnum(code[++index])) buffer[j++] = code[index];
             buffer[j] = '\0';
             if (strcmp(buffer, "int") == 0) plus_token("INT", buffer);
             else if (strcmp(buffer, "if") == 0) plus_token("IF", buffer);
             else plus_token("IDENTIFIER", buffer);
-        } else if (code[index] == '=') {
+        } else if (code[index] == '=')
+        
+        {
             if (code[index + 1] == '=') { plus_token("EQUAL", "=="); index++; }
             else plus_token("ASSIGN", "=");
         } else if
